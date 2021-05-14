e02bef387be785023b44ba125373bde02d4d9859
Aionmone <aionmone14@gmail.com>
2021-05-14 22:17:18 +0200

    Inherit form base class and del 'Stack' class

--- exceptions/stack.py
+++ exceptions/stack.py
@@ -1,19 +1,11 @@
-class Stack(Exception):
-    '''Basic stack exception'''
-    def __init__(self, message):
-        self.__message = message
+from exceptions.base import Base
 
-        super().__init__(self.__message)
-
-    def __str__(self):
-        return self.__message
-
-class StackFull(Stack):
+class StackFull(Base):
     '''Full stack exception'''
     def __init__(self, message='Stack has reached its full capacity.'):
         super().__init__(message)
 
-class StackEmpty(Stack):
+class StackEmpty(Base):
     '''Empty stack exception'''
     def __init__(self, message='Stack is empty.'):
         super().__init__(message)
