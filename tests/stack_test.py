e8fa0f40976b71434b089bc1775452059722b483
Aionmone <aionmone14@gmail.com>
2021-05-15 00:55:09 +0200

    Enhance Stack DS test case

--- tests/stack_test.py
+++ tests/stack_test.py
@@ -9,54 +9,53 @@ class StackTestCase(unittest.TestCase):
 
     def setUp(self):
         self.stack = Stack()
-        self.__fill_stack()
+        # self.__fill_stack()
 
     def test_push(self):
         '''Test for stack push'''
+        for i in range(0, self.stack.capacity):
+            self.assertIsNone(self.stack.push(i), msg="Shouldn't raise StackFull error")
+        
         with self.assertRaises(StackFull):
             self.stack.push(15)
 
-        self.__empty_stack()
-        self.assertIsNone(self.stack.push(1), msg='Should returns None')
-
     def test_pop(self):
         '''Test for stack pop'''
-        self.assertEqual(self.stack.peek, self.stack.pop(), msg='Should returns last item')
+        self.__fill_stack()
+        for i in range(len(self.stack)):
+            i += 1
+            i = self.stack.capacity - i
+            self.assertEqual(self.stack.pop(), i, msg="Shouldn't raise StackEmpty error")
 
-        self.__empty_stack()
         with self.assertRaises(StackEmpty):
             self.stack.pop()
 
     def test_isEmpty(self):
         '''Test for stack isEmpty'''
-        self.assertFalse(self.stack.isEmpty)
-
-        self.__empty_stack()
         self.assertTrue(self.stack.isEmpty)
+
+        self.stack.push(0)
+        self.assertFalse(self.stack.isEmpty)
 
     def test_isFull(self):
         '''Test for stack isFull'''
-        self.assertTrue(self.stack.isFull)
-
-        self.__empty_stack()
         self.assertFalse(self.stack.isFull)
+
+        self.__fill_stack()
+        self.assertTrue(self.stack.isFull)
 
     def test_peek(self):
         '''Test for stack peek'''
-        self.assertEqual(self.stack.peek, 14, msg='Should shows 1')
+        self.__fill_stack()
+        while len(self.stack) >= 1:
+            self.assertEqual(self.stack.peek, self.stack.pop(), msg='Should returns equal')
 
-        self.__empty_stack()
         with self.assertRaises(StackEmpty):
             self.stack.peek
 
-    def __empty_stack(self):
-        '''Empty Stack for testing'''
-        for i in range(0, len(self.stack)):
-            self.stack.pop()
-
     def __fill_stack(self):
         '''Fill stack for testing'''
-        for i in range(0, self.stack.capacity):
+        for i in range(self.stack.capacity):
             self.stack.push(i)
 
 if __name__ == '__main__':
