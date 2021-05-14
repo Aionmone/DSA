db174b9e54572012918d203435369954bd0a8546
Aionmone <aionmone14@gmail.com>
2021-05-14 22:13:27 +0200

    Import Queue DS

--- DSA/__init__.py
+++ DSA/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '0.0.1.a'
 
 from DSA.stack import Stack
-# from DSA.queue import Queue, CircularQueue, PriorityQueue, Deque
+from DSA.queue import Queue#, CircularQueue, PriorityQueue, Deque
