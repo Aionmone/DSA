507b877678a5ce9bffa6553b131dbafd2b8fbaf3
Aionmone <aionmone14@gmail.com>
2021-05-20 13:06:52 +0200

    Create Node

---
+++ DSA/linked_list/node.py
@@ -0,0 +1,20 @@
+# Node Class
+
+from typing import ItemsView
+# Node class implementation
+
+class Node:
+    """Node implementation for Linked list DS.
+    
+    Attributes:
+        data:
+            Data storage in node.
+        prev:
+            Pointer to the previous Node.
+        Next:
+            Pointer to the next Node.
+    """
+    def __init__(self, data) -> None:
+        self.item = data
+        self.prev: Node = None
+        self.next: Node = None
