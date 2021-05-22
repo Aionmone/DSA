a5df602db72069bec2e3f057f07906673eabb89c
Aionmone <aionmone14@gmail.com>
2021-05-22 16:43:29 +0200

    Add tests for push, append, insert_after methods

--- tests/linked_list/linked_list_test.py
+++ tests/linked_list/linked_list_test.py
@@ -33,27 +33,41 @@ class LinkedListTestCase(unittest.TestCase):
             test = next(iter_test)
             self.assertEqual(data, test, f"Should equal {test}")
 
-    def test_insert(self):
+    def test_push(self):
+        """Test LinkedList push method."""
+        for i in range(5):
+            self.assertIsNone(self.list.push(i))
+
+        test_case = (4, 3, 2, 1, 0)
+        self.assertEqual(self.list.display(), (4, 3, 2, 1, 0), f"Should equal {test_case}")
+
+    def test_append(self):
+        """Test LinkedList append method."""
+        list_num = 5
+        for i in range(list_num):
+            self.assertIsNone(self.list.append(i))
+
+        test_case = tuple(num for num in range(list_num))
+        self.assertEqual(self.list.display(), test_case, f"Should equal {test_case}")
+
+    def test_insert_after(self):
         """Test LinkedList insert function"""
         # Empty insert
-        self.assertIsNone(self.list.insert(0, 0))
+        self.assertIsNone(self.list.insert_after(0, 0))
         self.assertEqual(self.list.get(0), 0, "Should equal 0")
 
-        # Insert at head
-        self.assertIsNone(self.list.insert(1, 0))
-        self.assertEqual(self.list.get(0), 1, "Should equal 0")
+        # Insert
+        self.assertIsNone(self.list.insert_after(1, 0))
+        self.assertEqual(self.list.get(1), 1, "Should equal 1")
 
-        # Insert at tail
-        self.list.insert(2, len(self.list))
-        self.assertEqual(self.list.get(2), 2, "Should equal 2")
+        self.assertIsNone(self.list.insert_after(2, 0))
+        self.assertEqual(self.list.get(1), 2, "Should equal 2")
 
-        # Insert at middle
-        self.assertIsNone(self.list.insert(3, 1))
-        self.assertEqual(self.list.get(1), 3, "Should equal 3")
+        self.assertEqual(self.list.display(), (0, 2, 1), "Should equal (0, 2, 1)")
 
         # IndexError
         with self.assertRaises(IndexError, msg= "Should raises IndexError."):
-            self.list.insert(5, 5)
+            self.list.insert_after(5, 3)
 
     def test_delete(self):
         """Tests linked list delete method"""
@@ -82,4 +96,4 @@ class LinkedListTestCase(unittest.TestCase):
     def __insert(self, items):
         """Insert items at tail of linked list"""
         for i in range(items):
-            self.list.insert(i, len(self.list))
+            self.list.append(i)
